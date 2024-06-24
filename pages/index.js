ad9a69423b0437632e11fb6cf595e77ab9484943
Ony <onyrakoto27@gmail.com>
2024-06-25 00:32:58 +0300

    add sorting

--- pages/index.js
+++ pages/index.js
@@ -1,178 +1,28 @@
-import Link from "@/components/Link";
-import Arrow from "@/components/Arrow";
-import * as Collapsible from "@radix-ui/react-collapsible";
 import React from "react";
+import { artistData } from "../data";
+import { Sort } from "@/components/Sort";
+import { ArtistRow } from "@/components/ArtistRow";
 
-export default function Home({ parks }) {
-  const ParkRow = (props) => {
-    const {
-      parkName,
-      city,
-      country,
-      notableFeature,
-      latitude,
-      longitude,
-      locationLink,
-      description,
-      submittedByLink,
-      submittedByHandle,
-      suggestedBook,
-      suggestedBookLink,
-    } = props;
+export async function getServerSideProps(context) {
+  const { sortBy } = context.query;
 
-    return (
-      <Collapsible.Root>
-        <li className="transition hover:bg-sally/10">
-          <Collapsible.Trigger className="w-full grid text-left py-2.5 px-4 grid-cols-yeah group">
-            <h2>{parkName}</h2>
-            <div>{city}</div>
-            <div>{country}</div>
-            <div>{notableFeature}</div>
-            <Link
-              href={locationLink}
-              isExternal
-              className="z-10 p-1 ml-4 leading-none transition rounded-sm bg-sally/50 w-fit hover:bg-mcqueen hover:text-white tabular-nums text-sm"
-            >
-              "latitude"
-            </Link>
-            <div className="ml-auto transition-transform duration-300 group-data-[state='open']:rotate-90 place-self-center">
-              <Arrow />
-            </div>
-          </Collapsible.Trigger>
-          <Collapsible.Content className="overflow-hidden data-[state=open]:open data-[state=closed]:close">
-            <div className="grid px-4 overflow-hidden grid-cols-yeah">
-              <div className="flex flex-col col-span-2 pl-12 ml-1.5 gap-y-6 pb-6 pt-3">
-                <h3 className="sr-only">Description</h3>
-                <p>{description}</p>
-                <div className="flex leading-none gap-x-10">
-                  <div>
-                    <h3 className="text-sm mb-1.5">Recommended reading</h3>
-                    <Link
-                      href={suggestedBookLink}
-                      isExternal
-                      className="underline transition hover:bg-sally/50 underline-offset-4"
-                    >
-                      {suggestedBook}
-                    </Link>
-                  </div>
-                  <div>
-                    <h3 className="text-sm mb-1.5">Submitted by</h3>
-                    {submittedByHandle ? (
-                      <Link
-                        href={submittedByLink}
-                        isExternal
-                        className="underline transition hover:bg-sally/50 underline-offset-4"
-                      >
-                        @{submittedByHandle}
-                      </Link>
-                    ) : (
-                      <p>{submittedByLink}</p>
-                    )}
-                  </div>
-                  <Link
-                    href={`https://www.google.com/maps/search/?api=1&query@${encodeURIComponent(
-                      latitude + "," + longitude
-                    )}`}
-                    className="flex items-center self-end gap-x-1.5 underline transition hover:bg-sally/50 underline-offset-4"
-                    isExternal
-                  >
-                    Get directions
-                  </Link>
-                </div>
-              </div>
-            </div>
-          </Collapsible.Content>
-        </li>
-      </Collapsible.Root>
-    );
+  if (sortBy) {
+    artistData.sort(sortBy);
+  }
+
+  return {
+    props: {
+      artists: artistData.artists,
+    },
   };
+}
 
-  const MobileParkRow = (props) => {
-    const {
-      parkName,
-      city,
-      country,
-      latitude,
-      longitude,
-      locationLink,
-      description,
-      submittedByLink,
-      submittedByHandle,
-      suggestedBook,
-      suggestedBookLink,
-      id,
-    } = props;
-    return (
-      <Collapsible.Root>
-        <li>
-          <Collapsible.Trigger className="flex justify-center items-center gap-x-4 w-full pr-4 group">
-            <div className="bg-mcqueen text-white px-4 py-6 z-10 w-full max-w-[3.5rem] -mb-px tabular-nums">
-              "______"
-            </div>
-            <div className="flex flex-col text-left">
-              <span>{parkName}</span>
-              <span>
-                {city}, {country}
-              </span>
-            </div>
-            <div className="ml-auto transition-transform duration-300 group-data-[state='open']:rotate-90">
-              <Arrow />
-            </div>
-          </Collapsible.Trigger>
-          <Collapsible.Content className="data-[state=open]:open data-[state=closed]:close overflow-hidden">
-            <div className="flex flex-row">
-              <div className="bg-mcqueen h-max-content w-full max-w-[3.5rem]" />
-              <div className="px-4 pt-2">
-                <p className="max-w-prose">{description}</p>
-                <div className="flex flex-col mt-8 items-baseline leading-none bg-mcqueen/5 text-sm -mx-4 divide-y divide-mcqueen border-t border-mcqueen">
-                  <div className="w-full py-3 flex flex-row justify-between px-4">
-                    <h3>Recommended reading</h3>
-                    <Link
-                      href={suggestedBookLink}
-                      isExternal
-                      className="underline transition hover:bg-sally/50 underline-offset-4 w-fit"
-                    >
-                      {suggestedBook}
-                    </Link>
-                  </div>
-                  <div className="w-full py-3 flex flex-row justify-between px-4">
-                    <h3>Submitted by</h3>
-                    {submittedByHandle ? (
-                      <Link
-                        href={submittedByLink}
-                        isExternal
-                        className="underline transition hover:bg-sally/50 underline-offset-4 w-fit"
-                      >
-                        @{submittedByHandle}
-                      </Link>
-                    ) : (
-                      <p>{submittedByLink}</p>
-                    )}
-                  </div>
-                  <div className="w-full py-3 flex flex-row justify-between px-4">
-                    <h3>Directions</h3>
-                    <Link
-                      href={locationLink}
-                      isExternal
-                      className="underline transition hover:bg-sally/50 underline-offset-4 w-fit"
-                    >
-                      "latitude"
-                    </Link>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </Collapsible.Content>
-        </li>
-      </Collapsible.Root>
-    );
-  };
-
+export default function Home({ artists }) {
   return (
     <main className="w-full h-full">
-      <ul className="sm:hidden grid w-full divide-y divide-mcqueen border-t-2 border-mcqueen">
-          <MobileParkRow/>
-      </ul>
+      <div className="mb-4 px-3 flex justify-end">
+        <Sort />
+      </div>
       <div className="hidden sm:block">
         <div className="text-xl border-b-2 border-mcqueen">
           <ul className="grid w-full px-4 grid-cols-yeah">
@@ -184,13 +34,11 @@ export default function Home({ parks }) {
         </div>
         <div className="grid isolate">
           <ul className="z-10 border-b divide-y border-mcqueen divide-mcqueen park overlay">
-              <ParkRow />
+            {artists.map((artist) => (
+              <ArtistRow key={artist.name} {...artist} />
+            ))}
           </ul>
-          <div className="w-full h-full content">
-            <div className="grid w-full h-full px-4 grid-cols-yeah col-divider" />
-          </div>
         </div>
-        <div className="absolute grid w-full px-4 -translate-x-1/2 left-1/2 grid-cols-yeah col-divider h-fill-available" />
       </div>
     </main>
   );
